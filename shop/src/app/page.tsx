94f83f2bed7ee5094bafc4932cab050a2f84b2e4
Nguyen Chau <chau1282001@gmail.com>
2023-12-12 08:33:48 +0700

    wrapper

--- shop/src/app/page.tsx
+++ shop/src/app/page.tsx
@@ -1,19 +1,40 @@
 import MaxWithWrapper from '@/components/MaxWidthWrapper'
 import { Button, buttonVariants } from '@/components/ui/button'
+import { ArrowDownToLine, CheckCircle, Leaf } from 'lucide-react'
 import Link from 'next/link'
+
+const perks = [
+  {
+    name : 'Instant delivery',
+    Icon : ArrowDownToLine,
+    decription : 'Get your assets delivered to your email address in seconds and download them right away'
+  },
+  {
+    name : 'Guaranteed quantity',
+    Icon : CheckCircle,
+    decription : 'Every asset on our platform os verified by our team to ensure our highest quantity standards'
+  },
+  {
+    name : 'For the Planet',
+    Icon : Leaf,
+    decription : 'You can create new copies of the planet on your computer at any time and you can use them to create ....'
+  }
+]
+
 export default function Home() {
   return (
+    <>
 <MaxWithWrapper>
 <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
           <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
             Your marketplace for high-quality{' '}
             <span className='text-blue-600'>
-              digital assets
+            Products
             </span>
             .
           </h1>
           <p className='mt-6 text-lg max-w-prose text-muted-foreground'>
-            Welcome to DigitalHippo. Every asset on our
+            Welcome to QynCha. Every asset on our
             platform is verified by our team to ensure our
             highest quality standards.
           </p>
@@ -29,6 +50,34 @@ export default function Home() {
           </div>
         </div>
   </MaxWithWrapper> 
+
+  <section className='border-t border-gray-200 bg-gray-50'> 
+  <MaxWithWrapper classname='py-20'>
+  <div className='grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0'>
+            {perks.map((perk) => (
+              <div
+                key={perk.name}
+                className='text-center md:flex md:items-start md:text-left lg:block lg:text-center'>
+                <div className='md:flex-shrink-0 flex justify-center'>
+                  <div className='h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900'>
+                    {<perk.Icon className='w-1/3 h-1/3' />}
+                  </div>
+                </div>
+
+                <div className='mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6'>
+                  <h3 className='text-base font-medium text-gray-900'>
+                    {perk.name}
+                  </h3>
+                  <p className='mt-3 text-sm text-muted-foreground'>
+                    {perk.decription}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+    </MaxWithWrapper>  
+  </section> 
+  </>
   )
   
 }
